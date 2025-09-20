305a14d87268ddddda50beb23b5d6ba66b27946c
Manuel Doño <mdonolobo@gmail.com>
2025-09-20 15:52:39 -0600

    .

---
+++ 1842/archivo1.js
@@ -0,0 +1,20 @@
+//Operadores de comparación
+let valor1, valor2;
+valor1='20';
+valor2=10;
+
+document.write(valor1 > valor2);//mayor que
+document.write('<br>');
+document.write(valor1 < valor2);//menor que
+document.write('<br>');
+//document.write(valor1 = valor2);//asignación
+document.write('<br>');
+document.write(valor1 == valor2);//comparación
+document.write('<br>');
+document.write(valor1 === valor2);//indéntico
+document.write('<br>');
+
+document.write(valor1 >= valor2);//mayor que
+document.write('<br>');
+document.write(valor1 <= valor2);//menor que
+document.write('<br>');
