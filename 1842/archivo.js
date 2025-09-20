68dc1ace80a4892a5b37c18904428a11c80d7fe6
Manuel Doño <mdonolobo@gmail.com>
2025-09-20 11:05:35 -0600

    *
    primero

---
+++ 1842/archivo.js
@@ -0,0 +1,26 @@
+//document.write("Hola Mundo!!!");
+//document.write("Feliz Fin de Semana!!!");
+var nombre;//variable creada
+//nombre="Manuel";//variable inicializada
+nombre='Pedro';
+//tipo nombre valor
+// SET = COMPARACION == IDENTICO ===
+/*
+var Nombre='Juan';
+var NOMBRE='Elizabeth';
+var _nombre='Lucas';
+var $_nombre='Miguel';
+let saludo = 'Hola';
+
+//constante
+const A=10;
+//A=10;
+const IVA='12%';
+const _IVA='12%';
+const _IVA1='12%';
+const $_IVA='12%';
+
+//document.write(nombre);
+//document.write(saludo);
+//document.write($_IVA);
+document.write(A);*/
